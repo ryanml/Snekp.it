e16cb0f0ee81d4de92803b49e790a88fca6cf931
ryanml <rlanese@asu.edu>
2016-07-18 12:47:42 -0400

    fixed listen

--- app.js
+++ app.js
@@ -20,6 +20,4 @@ app.get('/', function(req, res) {
 socket.socket(http);
 
 // Set server to listen designated port
-http.listen(app.get('port'), '0.0.0.0', function() {
-  console.log('Listening on port ' + app.get('port'));
-});
+http.listen(process.env.PORT || app.get('port'));
