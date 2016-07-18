4d518888ce904d8257340db3e072d679174fa138
ryanml <rlanese@asu.edu>
2016-07-17 23:23:16 -0400

    Client side refactor, no longer using es6 class.

--- public/js/client.js
+++ public/js/client.js
@@ -1,275 +1,275 @@
-'use strict';
 window.onload = function() {
-  class GameHandler {
-    constructor(id) {
-      this.id = id;
-      this.viewportX;
-      this.viewportY;
-      this.headX;
-      this.headY;
-      this.gridWidth;
-      this.gridHeight;
-      this.gridSize = 300;
-      this.blockSize = 20;
-      this.action = false;
-      this.gameState = {};
-      this.domActions();
-    }
-    updateGameState(newState) {
-      this.gameState = newState;
-    }
-    checkLife() {
-      var player = this.gameState.players.filter(p => p.id === this.id);
-      return player.length === 0 ? false : true;
-    }
-    getScore() {
-      var player = this.gameState.players.filter(p => p.id === this.id);
-      return player[0].sLength;
-    }
-    setCellDimensions() {
-      var width = 0,
-        height = 0;
-      for (var w = 0; w < this.canvas.width; w += this.blockSize) {
-        width++;
-      }
-      for (var h = 0; h < this.canvas.height; h += this.blockSize) {
-        height++;
-      }
-      // Width and height of grid in cells
-      this.gridWidth = width;
-      this.gridHeight = height;
-    }
-    calculateViewport() {
-      // Get the position of the player's head block
-      var player = this.gameState.players.filter(p => p.id === this.id)[0];
-      this.headX = player.blocks[0][0], this.headY = player.blocks[0][1];
-      // Get offset from end of the grid for both x and y
-      this.viewportX = [this.headX - (this.gridWidth / 2), (this.headX + this.gridWidth)];
-      this.viewportY = [this.headY - (this.gridHeight / 2), (this.headY + this.gridHeight)];
-    }
-    drawState() {
-      // Helper functions
-      const calc = (num) => {
-        return num * this.blockSize;
-      };
-      const drawBounds = (x, y) => {
-        var gS = this.gridSize;
-        var gW = this.gridWidth,
-          gH = this.gridHeight;
-        var vX = this.viewportX[0],
-          vY = this.viewportY[0];
-        if (x < gS) {
-          for (var fy = ((gH / 2) * -1); fy < gH; fy++) {
-            for (var i = 0; i < (gW / 2); i++) {
-              this.context.strokeRect(calc((x - ((x + 1) + i)) - vX), calc((y - vY) + fy), this.blockSize, this.blockSize);
-            }
-          }
-        }
-        if ((gS - x) < gW) {
-          for (var fy = ((gH / 2) * -1); fy < gH; fy++) {
-            for (var i = 0; i < (gW / 2); i++) {
-              this.context.strokeRect(calc(((gS - x) + (gW / 2)) + i), calc((y - vY) + fy), this.blockSize, this.blockSize);
-            }
-          }
-        }
-        if (y < gW) {
-          for (var fx = ((gW / 2) * -1); fx < gW; fx++) {
-            for (var i = 1; i < (gW / 2); i++) {
-              this.context.strokeRect(calc((x - vX) + fx), calc((y - (y + i)) - vY), this.blockSize, this.blockSize);
-            }
-          }
-        }
-        if ((gS - y) < gH) {
-          for (var fx = ((gW / 2) * -1); fx < gW; fx++) {
-            for (var i = 0; i < (gW / 2); i++) {
-              this.context.strokeRect(calc((x - vX) + fx), calc(((gS - y) + (gH / 2)) + i), this.blockSize, this.blockSize);
-            }
-          }
-        }
-      };
-      const checkViewBounds = (coords) => {
-        if ((coords[0] >= this.viewportX[0] && coords[0] <= this.viewportX[1]) &&
-          (coords[1] >= this.viewportY[0] && coords[1] <= this.viewportY[1])) {
-          return true;
-        }
-        else {
-          return false;
-        }
-      };
-      // Clear old state
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-      // Draw bound lines
-      this.context.fillStyle = '#ffffff';
-      drawBounds(this.headX, this.headY);
-      this.context.fillStyle = '#d3d3d3';
-      // Draw food particles
-      var foodCoords = this.gameState.foodCoords;
-      for (var f = 0; f < foodCoords.length; f++) {
-        var coords = foodCoords[f].coords;
-        if (checkViewBounds(coords)) {
-          this.context.drawImage(this.foodImage, calc(coords[0] - this.viewportX[0]), calc(coords[1] - this.viewportY[0]));
-        }
-      }
-      // Draw shields
-      var shieldCoords = this.gameState.shieldCoords;
-      for (var s = 0; s < shieldCoords.length; s++) {
-        var coords = shieldCoords[s].coords;
-        if (checkViewBounds(coords)) {
-          this.context.drawImage(this.shieldImage, calc(coords[0] - this.viewportX[0]), calc(coords[1] - this.viewportY[0]));
-        }
-      }
-      // Draw player blocks
-      var players = this.gameState.players;
-      for (var p = 0; p < players.length; p++) {
-        for (var b = 0; b < players[p].blocks.length; b++) {
-          var blocks = players[p].blocks;
-          if (checkViewBounds(blocks[b])) {
-            this.context.fillStyle = players[p].color;
-            this.context.strokeRect(calc(blocks[b][0] - this.viewportX[0]), calc(blocks[b][1] - this.viewportY[0]), this.blockSize, this.blockSize);
-            // If the player is immune, use the rainbow immune image for the blocks
-            if (players[p].immunity > 0) {
-              this.context.drawImage(this.immuneImage, calc(blocks[b][0] - this.viewportX[0]), calc(blocks[b][1] - this.viewportY[0]));
-            }
-            else {
-              this.context.fillRect(calc(blocks[b][0] - this.viewportX[0]), calc(blocks[b][1] - this.viewportY[0]), this.blockSize, this.blockSize);
-            }
-          }
-        }
-        // Draw the nickname under the head
-        this.context.fillStyle = '#000000';
-        this.context.fillText(players[p].nick, calc(blocks[0][0] - this.viewportX[0]) - 20, calc(blocks[0][1] - this.viewportY[0]) + 35);
-        this.context.strokeStyle = '#d3d3d3';
-      }
-      // Call stat boxes
-      this.drawStatBoxes();
-    }
-    drawStatBoxes() {
-      // Set box attributes
-      this.context.globalAlpha = 0.8;
-      this.context.fillStyle = '#5A5A5A';
-      this.context.font = '20px monospace';
-      // Draw leaderboard box
-      this.context.fillRect(this.canvas.width - 190, 10, 180, 260);
-      // Draw stats box
-      this.context.fillRect(10, this.canvas.height - 50, 285, 30);
-      // Draw placeholder leaderboard
-      this.context.fillStyle = '#ffffff';
-      this.context.fillText('Leaderboard', this.canvas.width - 170, 30);
-      // Draw leaders
-      var leaders = this.gameState.leaders;
-      var x = this.canvas.width - 175,
-        y = 70,
-        yInc = 30;
-      for (var l = 0; l < leaders.length; l++) {
-        var pos = l + 1;
-        var pNick = leaders[l].nick
-        var leadString = pos + '. ' + pNick;
-        this.context.fillText(leadString, x, y);
-        y += yInc;
-      }
-      // Draw score and position
-      this.context.fillStyle = '#ffffff';
-      this.context.font = '15px monospace';
-      var scoreString = 'Score: ' + score;
-      var posString = 'Position: (' + this.headX + ',' + this.headY + ')';
-      this.context.fillText(scoreString, 20, this.canvas.height - 30);
-      this.context.fillText(posString, 120, this.canvas.height - 30);
-      // Reset opacity and fillstyle
-      this.context.globalAlpha = 1.0;
-      this.context.fillStyle = '#d3d3d3';
-    }
-    sendAction(e) {
-      var key = e.keyCode;
-      switch (key) {
-        case 37:
-          if (this.action !== 'RIGHT' || score === 1) {
-            this.action = 'LEFT';
-          }
-          break;
-        case 38:
-          if (this.action !== 'DOWN' || score === 1) {
-            this.action = 'UP';
-          }
-          break;
-        case 39:
-          if (this.action !== 'LEFT' || score === 1) {
-            this.action = 'RIGHT';
-          }
-          break;
-        case 40:
-          if (this.action !== 'UP' || score === 1) {
-            this.action = 'DOWN';
-          }
-          break;
-      }
-      if (this.action) {
-        socket.emit('player-movement', this.action);
-      }
-    }
-    domActions() {
-      this.foodImage = document.createElement('img');
-      this.foodImage.src = '/img/food.png';
-      this.shieldImage = document.createElement('img');
-      this.shieldImage.src = '/img/shield.png';
-      this.immuneImage = document.createElement('img');
-      this.immuneImage.src = '/img/immune.jpg';
-      this.canvas = document.getElementById('game-canvas');
-      this.context = this.canvas.getContext('2d');
-      this.context.strokeStyle = '#d3d3d3';
-      this.replayDiv = document.getElementById('replay-div');
-      this.replayButton = document.getElementById('replay');
-      this.replayButton.addEventListener('click', this.reloadPage);
-      document.body.addEventListener('keydown', this.sendAction);
-    }
-    reloadPage() {
-      location.reload();
-    }
-  }
-  var nick,
-    score,
-    clientId,
-    gameHandler,
-    headPosition;
+  'use strict';
+  // Need variables
+  var gameState;
   var socket = io();
+  var gridSize = 300;
+  var blockSize = 20;
+  var isPlay = false;
+  var viewportX, viewportY;
+  var playerHeadX, playerHeadY;
+  var widthInCells, heightInCells;
+  var playerId, playerNick, playerScore, playerAction;
+  // Needed dom elements
+  var body = document.body;
   var canvas = document.getElementById('game-canvas');
-  // Adjust on resize
+  var context = canvas.getContext('2d');
+  var replayDiv = document.getElementById('replay-div');
+  var replayButton = document.getElementById('replay');
+  // Create image elements
+  var foodImage = document.createElement('img');
+  foodImage.src = '/img/food.png';
+  var shieldImage = document.createElement('img');
+  shieldImage.src = '/img/shield.png';
+  var immuneImage = document.createElement('img');
+  immuneImage.src = '/img/immune.jpg';
+  // Set canvas width
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  // Add listener to readjust width and height on resize
   window.onresize = () => {
-    var width = (window.innerWidth);
-    var height = (window.innerHeight);
-    canvas.width = width;
-    canvas.height = height;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
   };
-  // Determine proper width/height for canvas
-  var width = (window.innerWidth);
-  var height = (window.innerHeight);
-  canvas.width = width;
-  canvas.height = height;
+  // Socket actions
   // Get player's client id
   socket.on('request-nick', function(id) {
-    if (!clientId) {
-      clientId = id;
-      while (!nick) {
-        nick = prompt("Enter a nickname: ");
+    if (!playerId) {
+      playerId = id;
+      while (!playerNick) {
+        playerNick = prompt("Enter a nickname: ");
       }
-      socket.emit('receive-nick', nick);
+      // Send nickname
+      socket.emit('receive-nick', playerNick);
     }
   });
   socket.on('received-nick', function() {
-    gameHandler = new GameHandler(clientId);
+    isPlay = true;
   });
-  // Action on state change
+  // Accept state change from socket
   socket.on('state-change', function(newState) {
-    if (gameHandler) {
-      gameHandler.setCellDimensions();
-      gameHandler.updateGameState(newState);
-      if (gameHandler.checkLife()) {
-        score = gameHandler.getScore();
-        gameHandler.calculateViewport();
-      }
-      else {
-        gameHandler.replayDiv.style.display = 'block';
-      }
-      gameHandler.drawState();
-    }
+    handleStateChange(newState);
   });
+  // Calls appropriate functions to handle state change
+  const handleStateChange = (newState) => {
+    if (isPlay) {
+      gameState = newState;
+      setCellDimensions();
+      if (checkLife()) {
+        setPlayerScore();
+        setPlayerPos();
+        calculateViewport();
+      } else {
+        promptDeath();
+      }
+      drawState();
+    }
+  };
+  // Sets the grid width and height in cells given the size
+  const setCellDimensions = () => {
+    var width = 0, height = 0;
+    for (var w = 0; w < canvas.width; w += blockSize) {
+      width++;
+    }
+    for (var h = 0; h < canvas.height; h += blockSize) {
+      height++;
+    }
+    widthInCells = width;
+    heightInCells = height;
+  };
+  // Checks if player is alive
+  const checkLife = () => {
+    var player = gameState.players.filter(p => p.id === playerId);
+    return player.length === 0 ? false : true;
+  };
+    // Sets the player score
+  const setPlayerScore = () => {
+    var player = gameState.players.filter(p => p.id === playerId);
+    playerScore = player[0].sLength;
+  };
+  // Sets the player position
+  const setPlayerPos = () => {
+    var player = gameState.players.filter(p => p.id === playerId)[0];
+    playerHeadX = player.blocks[0][0], playerHeadY = player.blocks[0][1];
+  };
+  // Calculates the viewport to display
+  const calculateViewport = () => {
+    // Get offset from end of the grid for both x and y
+    viewportX = [playerHeadX - (widthInCells / 2), (playerHeadX + widthInCells)];
+    viewportY = [playerHeadY - (heightInCells / 2), (playerHeadY + heightInCells)];
+  };
+  // Quick function to multiply by blocksize
+  const calc = (num) => {
+    return num * blockSize;
+  };
+  // Draws bound areas around the grid
+  const drawBounds = () => {
+    var gS = gridSize;
+    var x = playerHeadX, y = playerHeadY;
+    var vX = viewportX[0], vY = viewportY[0];
+    var gW = widthInCells, gH = heightInCells;
+    context.fillStyle = '#d3d3d3';
+    if (x < gS) {
+      for (var fy = ((gH / 2) * -1); fy < gH; fy++) {
+        for (var i = 0; i < (gW / 2); i++) {
+          context.strokeRect(calc((x - ((x + 1) + i)) - vX), calc((y - vY) + fy), blockSize, blockSize);
+        }
+      }
+    }
+    if ((gS - x) < gW) {
+      for (var fy = ((gH / 2) * -1); fy < gH; fy++) {
+        for (var i = 0; i < (gW / 2); i++) {
+          context.strokeRect(calc(((gS - x) + (gW / 2)) + i), calc((y - vY) + fy), blockSize, blockSize);
+        }
+      }
+    }
+    if (y < gW) {
+      for (var fx = ((gW / 2) * -1); fx < gW; fx++) {
+        for (var i = 1; i < (gW / 2); i++) {
+          context.strokeRect(calc((x - vX) + fx), calc((y - (y + i)) - vY), blockSize, blockSize);
+        }
+      }
+    }
+    if ((gS - y) < gH) {
+      for (var fx = ((gW / 2) * -1); fx < gW; fx++) {
+        for (var i = 0; i < (gW / 2); i++) {
+          context.strokeRect(calc((x - vX) + fx), calc(((gS - y) + (gH / 2)) + i), blockSize, blockSize);
+        }
+      }
+    }
+    context.fillStyle = '#ffffff';
+  };
+  // Checks if given coordinate is within viewport bounds
+  const checkViewBounds = (coords) => {
+    if ((coords[0] >= viewportX[0] && coords[0] <= viewportX[1]) && (coords[1] >= viewportY[0] && coords[1] <= viewportY[1])) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  // Draws player nick under head
+  const drawPlayerNick = (nick, headX, headY) => {
+    // Draw the nickname under the head
+    context.fillStyle = '#000000';
+    context.fillText(nick, calc(headX - viewportX[0]) - 20, calc(headY - viewportY[0]) + 35);
+    context.strokeStyle = '#d3d3d3';
+  };
+  // Draw stat boxes
+  const drawStatBoxes = () => {
+    // Set box attributes
+    context.globalAlpha = 0.8;
+    context.fillStyle = '#5A5A5A';
+    context.font = '20px monospace';
+    // Draw leaderboard box
+    context.fillRect(canvas.width - 190, 10, 180, 260);
+    // Draw stats box
+    context.fillRect(10, canvas.height - 50, 285, 30);
+    // Draw placeholder leaderboard
+    context.fillStyle = '#ffffff';
+    context.fillText('Leaderboard', canvas.width - 170, 30);
+    // Draw leaders
+    var leaders = gameState.leaders;
+    var x = canvas.width - 175,
+      y = 70,
+      yInc = 30;
+    for (var l = 0; l < leaders.length; l++) {
+      var pos = l + 1;
+      var pNick = leaders[l].nick
+      var leadString = pos + '. ' + pNick;
+      context.fillText(leadString, x, y);
+      y += yInc;
+    }
+    // Draw score and position
+    var scoreString = 'Score: ' + playerScore;
+    var posString = 'Position: (' + playerHeadX + ',' + playerHeadY + ')';
+    context.fillStyle = '#ffffff';
+    context.font = '15px monospace';
+    context.fillText(scoreString, 20, canvas.height - 30);
+    context.fillText(posString, 120, canvas.height - 30);
+    // Reset opacity and fillstyle
+    context.globalAlpha = 1.0;
+    context.fillStyle = '#d3d3d3';
+  };
+  // Draws all objects and players in the current state
+  const drawState = () => {
+    // Clear canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // Call function to draw bound lines
+    drawBounds();
+    // Draw food particles
+    var foodCoords = gameState.foodCoords;
+    for (var f = 0; f < foodCoords.length; f++) {
+      var coords = foodCoords[f].coords;
+      if (checkViewBounds(coords)) {
+        context.drawImage(foodImage, calc(coords[0] - viewportX[0]), calc(coords[1] - viewportY[0]));
+      }
+    }
+    // Draw shields
+    var shieldCoords = gameState.shieldCoords;
+    for (var s = 0; s < shieldCoords.length; s++) {
+      var coords = shieldCoords[s].coords;
+      if (checkViewBounds(coords)) {
+        context.drawImage(shieldImage, calc(coords[0] - viewportX[0]), calc(coords[1] - viewportY[0]));
+      }
+    }
+    // Draw player blocks
+    var players = gameState.players;
+    for (var p = 0; p < players.length; p++) {
+      for (var b = 0; b < players[p].blocks.length; b++) {
+        var blocks = players[p].blocks;
+        if (checkViewBounds(blocks[b])) {
+          context.fillStyle = players[p].color;
+          context.strokeRect(calc(blocks[b][0] - viewportX[0]), calc(blocks[b][1] - viewportY[0]), blockSize, blockSize);
+          // If the player is immune, use the rainbow immune image for the blocks
+          if (players[p].immunity > 0) {
+            context.drawImage(immuneImage, calc(blocks[b][0] - viewportX[0]), calc(blocks[b][1] - viewportY[0]));
+          } else {
+            context.fillRect(calc(blocks[b][0] - viewportX[0]), calc(blocks[b][1] - viewportY[0]), blockSize, blockSize);
+          }
+        }
+      }
+      drawPlayerNick(players[p].nick, blocks[0][0], blocks[0][1]);
+    }
+    drawStatBoxes();
+  };
+  // Sends key action to the socket
+  const sendAction = (e) => {
+      var key = e.keyCode;
+      switch (key) {
+        case 37:
+          if (playerAction !== 'RIGHT' || playerScore === 1) {
+            playerAction = 'LEFT';
+          }
+          break;
+        case 38:
+          if (playerAction !== 'DOWN' || playerScore === 1) {
+            playerAction = 'UP';
+          }
+          break;
+        case 39:
+          if (playerAction !== 'LEFT' || playerScore === 1) {
+            playerAction = 'RIGHT';
+          }
+          break;
+        case 40:
+          if (playerAction !== 'UP' || playerScore === 1) {
+            playerAction = 'DOWN';
+          }
+          break;
+      }
+      if (playerAction) {
+        socket.emit('player-movement', playerAction);
+      }
+    }
+    // Shows reload box
+  const promptDeath = () => {
+    replayDiv.style.display = 'block';
+  };
+  // Reloads page
+  const reloadPage = () => {
+    location.reload();
+  };
+  // Add events
+  body.addEventListener('keydown', sendAction);
+  replayButton.addEventListener('click', reloadPage);
 }
